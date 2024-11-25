776b2e82b421aff4154a7ea58590d73fbf1101e6
009MHz <mharisbsatriyo@gmail.com>
2024-11-26 06:13:28 +0700

    feat: complex flow locator completion

--- elements/__inventory.py
+++ elements/__inventory.py
@@ -29,3 +29,46 @@ class Products:
     price = "//div[@class='inventory_item_price']"
     add_btn = "//button[contains(@id,'add')]"
     remove_btn = "//button[contains(@id,'remove')]"
+
+
+class YourCart:
+    url = "https://www.saucedemo.com/cart.html"
+    section_title = ".title"
+    item_holder = ".cart_item_label"
+    item_name = "//div[@class='inventory_item_name']"
+    item_info = "//div[@class='inventory_item_desc']"
+    item_price = "//div[@class='inventory_item_price']"
+    item_remove = "//button[contains(@id,'remove')]"
+    back_btn = "#continue-shopping"
+    checkout_btn = "#checkout"
+
+
+class BuyInfo:
+    url = "https://www.saucedemo.com/checkout-step-one.html"
+    section_title = ".title"
+    main_form = ".checkout_info"
+    first_name = "#first-name"
+    last_name = "#last-name"
+    postal = "#postal-code"
+    cancel_btn = "#cancel"
+    continue_btn = "#continue"
+
+
+class BuyPreview:
+    url = "https://www.saucedemo.com/checkout-step-two.html"
+    section_title = ".title"
+    item_name = "//div[@class='inventory_item_name']"
+    item_info = "//div[@class='inventory_item_desc']"
+    item_price = "//div[@class='inventory_item_price']"
+    summary_info = ".summary_info"
+    cancel_btn = "#cancel"
+    finish_btn = "#finish"
+
+
+class BuyComplete:
+    url = "https://www.saucedemo.com/checkout-complete.html"
+    section_title = ".title"
+    main_display = "#checkout_complete_container"
+    header = ".complete-header"
+    info = ".complete-text"
+    back_home_btn = "#back-to-products"
