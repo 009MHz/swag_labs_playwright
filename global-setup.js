498f9df96e03ab3d93dc86c0e85f7e80197391b7
009MHz <mharisbsatriyo@gmail.com>
2024-12-08 18:06:22 +0700

    chore: revert to clean setup

--- global-setup.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const BrowserConfig = require('./utils/browserConfig');
-
-module.exports = async () => {
-    const browserConfig = new BrowserConfig();
-    await browserConfig.setupBrowser();
-    global.browserConfig = browserConfig;
-};
