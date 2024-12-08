ca72779649bdaa1d7cc2366e574bb984b593debe
009MHz <mharisbsatriyo@gmail.com>
2024-12-08 18:25:11 +0700

    feat: example test init

---
+++ tests/example.test.js
@@ -0,0 +1,19 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('Playwright Test Suite', () => {
+
+    test('Main Page should contain "Playwright" title', async ({ page }) => {
+        await page.goto('https://playwright.dev/');
+        // Expect a title "to contain" a substring.
+        await expect(page).toHaveTitle(/Playwright/);
+    });
+
+    test('Main page component should be loaded', async ({ page }) => {
+        await page.goto('https://playwright.dev/');
+        // Click the get started link.
+        await page.getByRole('link', { name: 'Get started' }).click();
+        // Expects page to have a heading with the name of Installation.
+        await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    });
+
+});
